18db716a60b7afc461a11b6944aaa18ee6cf1fad
Sérgio Serra <sergioserra.2003@gmail.com>
2025-12-14 21:01:03 +0000

    FreeIPA Container

--- setup_freeipa.sh
+++ setup_freeipa.sh
@@ -27,18 +27,22 @@ echo ""
 read -p "Press Enter to execute podman run..."
 
 # Execute podman command
-# podman run --name freeipa-server-container -ti \
-#     -h "${HOSTNAME}" --read-only \
-#     -v "$(pwd)/ipa-data:/data:Z" \
-#     -p 53:53/udp -p 53:53 \
-#     -p 80:80 -p 443:443 \
-#     -p 389:389 -p 636:636 \
-#     -p 88:88 -p 464:464 \
-#     -p 88:88/udp -p 464:464/udp \
-#     -p 123:123/udp \
-#     quay.io/freeipa/freeipa-server:rocky-9 \
-#     ipa-server-install -U \
-#     --realm="${REALM}" \
-#     --ds-password="${DS_PASSWORD}" \
-#     --admin-password="${ADMIN_PASSWORD}" \
-#     --no-ntp
+podman run --name netzor-freeipa-server -ti \
+    -h ${HOSTNAME} --read-only \
+    -v $(pwd)/ipa-data:/data:Z \
+    -p 53:53/udp -p 53:53 \
+    -p 80:80 -p 443:443 \
+    -p 389:389 -p 636:636 \
+    -p 88:88 -p 464:464 \
+    -p 88:88/udp -p 464:464/udp \
+    -p 123:123/udp \
+    --dns=127.0.0.1,1.1.1.1,8.8.8.8 \
+    quay.io/freeipa/freeipa-server:rocky-9 \
+    ipa-server-install -U \
+    --realm=${REALM} \
+    --ds-password=${DS_PASSWORD} \
+    --admin-password=${ADMIN_PASSWORD} \
+    --no-ntp \
+    --setup-dns \
+    --auto-forwarders \
+    --allow-zone-overlap
