3c868a23d1109a585544e8260be113adc216f524
Sérgio Serra <sergioserra.2003@gmail.com>
2026-01-05 20:39:09 +0000

    refactor: Dynamically configure BunkerWeb reverse proxy settings and server names, and ensure network creation is idempotent.

--- bunkerweb/setup.sh
+++ bunkerweb/setup.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
+set -e
 
-podman network create waf
+# Criar network se não existir
+podman network exists waf || podman network create waf
 
 WAF_DNS=$(podman network inspect waf --format '{{(index .Subnets 0).Gateway}}')
 
+# Lista de serviços: HOSTNAME=BACKEND_URL
+SERVICES=(
+  "${FREEIPA_HOSTNAME}=https://freeipa"
+  "${KEYCLOAK_HOSTNAME}=http://keycloak:8080"
+  "${ROUNDCUBE_HOSTNAME}=http://roundcube"
+  "${NEXTCLOUD_HOSTNAME}=http://nextcloud-web"
+  "${GLPI_HOSTNAME}=http://glpi"
+  "${IRIS_HOSTNAME}=https://iriswebapp_nginx:8443"
+  "${N8N_HOSTNAME}=http://n8n_app:5678"
+  "${GUACAMOLE_HOSTNAME}=http://guacamole:8080"
+)
+
+# Variáveis dinâmicas do BunkerWeb
+BUNKER_ENV=()
+
+SERVER_NAMES=()
+
+for SERVICE in "${SERVICES[@]}"; do
+  HOST="${SERVICE%%=*}"
+  BACKEND="${SERVICE#*=}"
+
+  SERVER_NAMES+=("$HOST")
+
+  BUNKER_ENV+=(
+    -e "${HOST}_USE_REVERSE_PROXY=yes"
+    -e "${HOST}_REVERSE_PROXY_HOST=${BACKEND}"
+    -e "${HOST}_SECURITY_MODE=detect"
+  )
+done
+
 podman run -d \
-    --name bunkerweb \
-    --network=waf \
-    -e DNS_RESOLVERS="${WAF_DNS}" \
-    -p 80:8080/tcp \
-    -p 443:8443/tcp \
-    -p 443:8443/udp \
-    -v bunkerweb:/data:Z \
-    -e ADMIN_USERNAME=${BUNKERWEB_ADMIN_USERNAME} \
-    -e ADMIN_PASSWORD=${BUNKERWEB_ADMIN_PASSWORD} \
-    -e USE_CROWDSEC=yes \
-    -e USE_WHITELIST=yes \
-    -e WHITELIST_COUNTRY="PT" \
-    -e MULTISITE=yes \
-    -e SERVER_NAME="${FREEIPA_HOSTNAME} ${KEYCLOAK_HOSTNAME} ${ROUNDCUBE_HOSTNAME} ${NEXTCLOUD_HOSTNAME} ${GLPI_HOSTNAME} ${IRIS_HOSTNAME} ${N8N_HOSTNAME} ${GUACAMOLE_HOSTNAME}" \
-    -e "${FREEIPA_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${FREEIPA_HOSTNAME}_REVERSE_PROXY_HOST=https://freeipa" \
-    -e "${FREEIPA_HOSTNAME}_SECURITY_MODE=detect" \
-    -e "${KEYCLOAK_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${KEYCLOAK_HOSTNAME}_REVERSE_PROXY_HOST=http://keycloak:8080" \
-    -e "${KEYCLOAK_HOSTNAME}_SECURITY_MODE=detect" \
-    -e "${ROUNDCUBE_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${ROUNDCUBE_HOSTNAME}_REVERSE_PROXY_HOST=http://roundcube" \
-    -e "${ROUNDCUBE_HOSTNAME}_SECURITY_MODE=detect" \
-    -e "${NEXTCLOUD_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${NEXTCLOUD_HOSTNAME}_REVERSE_PROXY_HOST=http://nextcloud-web" \
-    -e "${NEXTCLOUD_HOSTNAME}_SECURITY_MODE=detect" \
-    -e "${GLPI_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${GLPI_HOSTNAME}_REVERSE_PROXY_HOST=http://glpi" \
-    -e "${GLPI_HOSTNAME}_SECURITY_MODE=detect" \
-    -e "${IRIS_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${IRIS_HOSTNAME}_REVERSE_PROXY_HOST=https://iriswebapp_nginx:8443" \
-    -e "${IRIS_HOSTNAME}_SECURITY_MODE=detect" \
-    -e "${N8N_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${N8N_HOSTNAME}_REVERSE_PROXY_HOST=http://n8n_app:5678" \
-    -e "${N8N_HOSTNAME}_SECURITY_MODE=detect" \
-    -e "${GUACAMOLE_HOSTNAME}_USE_REVERSE_PROXY=yes" \
-    -e "${GUACAMOLE_HOSTNAME}_REVERSE_PROXY_HOST=http://guacamole:8080" \
-    -e "${GUACAMOLE_HOSTNAME}_SECURITY_MODE=detect" \
-    docker.io/bunkerity/bunkerweb-all-in-one:1.6.6
+  --name bunkerweb \
+  --network waf \
+  -e DNS_RESOLVERS="${WAF_DNS}" \
+  -p 80:8080/tcp \
+  -p 443:8443/tcp \
+  -p 443:8443/udp \
+  -v bunkerweb:/data:Z \
+  -e ADMIN_USERNAME="${BUNKERWEB_ADMIN_USERNAME}" \
+  -e ADMIN_PASSWORD="${BUNKERWEB_ADMIN_PASSWORD}" \
+  -e USE_CROWDSEC=yes \
+  -e USE_WHITELIST=yes \
+  -e WHITELIST_COUNTRY="PT" \
+  -e MULTISITE=yes \
+  -e SERVER_NAME="${SERVER_NAMES[*]}" \
+  "${BUNKER_ENV[@]}" \
+  docker.io/bunkerity/bunkerweb-all-in-one:1.6.6
