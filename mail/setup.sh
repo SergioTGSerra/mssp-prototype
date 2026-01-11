c8bb076691e7e899017b915c0e24e2d52b63834d
Sérgio Serra <sergioserra.2003@gmail.com>
2026-01-11 19:44:54 +0000

    Bugs https

--- mail/setup.sh
+++ mail/setup.sh
@@ -45,8 +45,8 @@ if ! podman exec keycloak /opt/keycloak/bin/kcadm.sh get clients -r netzor -q cl
         -s enabled=true \
         -s clientAuthenticatorType=client-secret \
         -s secret="${ROUNDCUBE_OIDC_CLIENT_SECRET}" \
-        -s "redirectUris=[\"https://${ROUNDCUBE_HOSTNAME}/*\"]" \
-        -s "webOrigins=[\"https://${ROUNDCUBE_HOSTNAME}\"]" \
+        -s "redirectUris=[\"https://${ROUNDCUBE_HOSTNAME}/*\", \"http://${ROUNDCUBE_HOSTNAME}/*\"]" \
+        -s "webOrigins=[\"https://${ROUNDCUBE_HOSTNAME}\", \"http://${ROUNDCUBE_HOSTNAME}\"]" \
         -s publicClient=false \
         -s protocol=openid-connect \
         -s 'defaultClientScopes=["openid", "profile", "email"]' > /dev/null 2>&1
