28c1aeb576b23bf83683e4cf2bf24aa53c4da446
Sérgio Serra <sergioserra.2003@gmail.com>
2026-02-08 11:45:01 +0000

    feat: Automate Frappe site creation and configuration and update ERPNext version

--- frappe/setup.sh
+++ frappe/setup.sh
@@ -12,3 +12,8 @@ podman-compose --env-file .env \
     -f overrides/compose.redis.yaml \
     -f overrides/compose.noproxy.yaml \
     up -d
+
+
+# podman exec frappe-backend bench new-site erp.netzor.pt --admin-password=admin --db-root-password=123 --install-app erpnext
+# podman exec frappe-backend bench --site erp.netzor.pt set-config host_name "https://erp.netzor.pt"
+# podman exec frappe-backend bench use erp.netzor.pt
