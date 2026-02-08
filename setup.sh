c195656144b62c65999f7c0d65bf9077ada1b712
Sérgio Serra <sergioserra.2003@gmail.com>
2026-02-08 14:27:05 +0000

    Refactor: restructure project directories with numerical prefixes for better organization.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ source ./utils.sh
 set -a; source .env; set +a
 
 # 1. Setup Prerequisites (OpenSSL, Podman)
-echo ">> Step 1: Checking/Installing Prerequisites..."
+echo ">> Step 0: Checking/Installing Prerequisites..."
 if [ -f "./prerequisites.sh" ]; then
     ./prerequisites.sh
     if [ $? -ne 0 ]; then
@@ -14,92 +14,26 @@ if [ -f "./prerequisites.sh" ]; then
     fi
 fi
 
-# 2. Setup BunkerWeb (WAF / Reverse Proxy)
-echo ">> Step 2: Installing BunkerWeb (WAF)..."
-if [ -f "./bunkerweb/setup.sh" ]; then
-    ./bunkerweb/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: BunkerWeb setup failed. Exiting."
-        exit 1
+# Loop through all numbered folders (01_*, 02_*, ..., nn_*)
+for folder in $(ls -d [0-9][0-9]_*/ 2>/dev/null | sort); do
+    # Remove trailing slash
+    folder="${folder%/}"
+    
+    # Extract step number and service name
+    step_num="${folder%%_*}"
+    service_name="${folder#*_}"
+    
+    echo ">> Step ${step_num}: Installing ${service_name}..."
+    
+    if [ -f "./${folder}/setup.sh" ]; then
+        ./${folder}/setup.sh
+        if [ $? -ne 0 ]; then
+            echo "Error: ${service_name} setup failed. Exiting."
+            exit 1
+        fi
+    else
+        echo "Warning: No setup.sh found in ${folder}, skipping..."
     fi
-fi
+done
 
-# 3. Setup FreeIPA
-echo ">> Step 3: Installing FreeIPA..."
-if [ -f "./freeipa/setup.sh" ]; then
-    ./freeipa/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: FreeIPA setup failed. Exiting."
-        exit 1
-    fi
-fi
-
-# 4. Setup Keycloak
-echo ">> Step 4: Installing Keycloak..."
-if [ -f "./keycloak/setup.sh" ]; then
-    ./keycloak/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: Keycloak setup failed. Exiting."
-        exit 1
-    fi
-fi
-
-# 5. Setup Mail Services (Mailserver + Roundcube)
-echo ">> Step 5: Installing Mail Services..."
-if [ -f "./mail/setup.sh" ]; then
-    ./mail/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: Mail Services setup failed. Exiting."
-        exit 1
-    fi
-fi
-
-# 6. Setup Frappe
-echo ">> Step 6: Installing Frappe..."
-if [ -f "./frappe/setup.sh" ]; then
-    ./frappe/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: Frappe setup failed. Exiting."
-        exit 1
-    fi
-fi
-
-# 7. Setup GLPI
-echo ">> Step 7: Installing GLPI..."
-if [ -f "./glpi/setup.sh" ]; then
-    ./glpi/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: GLPI setup failed. Exiting."
-        exit 1
-    fi
-fi
-
-# 8. Setup IRIS
-echo ">> Step 8: Installing IRIS..."
-if [ -f "./iris/setup.sh" ]; then
-    ./iris/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: IRIS setup failed. Exiting."
-        exit 1
-    fi
-fi
-
-# 9. Setup Jumpserver
-echo ">> Step 9: Installing Jumpserver..."
-if [ -f "./jumpserver/setup.sh" ]; then
-    ./jumpserver/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: Jumpserver setup failed. Exiting."
-        exit 1
-    fi
-fi
-
-# 10. Setup Nextcloud
-echo ">> Step 10: Installing Nextcloud..."
-if [ -f "./nextcloud-aio/setup.sh" ]; then
-    ./nextcloud-aio/setup.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: Nextcloud setup failed. Exiting."
-        exit 1
-    fi
-fi
+echo ">> All services installed successfully!"
