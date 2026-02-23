2972a9b98307f9a7af29b7a0e076f6117c2ca7b8
Sérgio Serra <sergioserra.2003@gmail.com>
2026-02-23 16:38:53 +0000

    feat: Implement Jumpserver LDAP authentication with FreeIPA, update environment variables for bind credentials

--- 08_jumpserver/setup.sh
+++ 08_jumpserver/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source utils.sh; script_init;
 
+# Create jumpserver-bind system user in FreeIPA
+freeipa_create_system_account "jumpserver-bind" "JumpServer" "Bind" "${JUMPSERVER_LDAP_BIND_PASSWORD}" "JumpServer Bind System Account"
+
 # Criar networks se não existirem
 podman network exists jumpserver_default || podman network create jumpserver_default
 
@@ -15,6 +18,13 @@ else
      --network=jumpserver_default \
      -e SECRET_KEY=PleaseChangeMe \
      -e BOOTSTRAP_TOKEN=PleaseChangeMe \
+     -e AUTH_LDAP=true \
+     -e AUTH_LDAP_SERVER_URI="ldap://${FREEIPA_HOSTNAME}" \
+     -e AUTH_LDAP_BIND_DN="${JUMPSERVER_LDAP_BIND_DN}" \
+     -e AUTH_LDAP_BIND_PASSWORD="${JUMPSERVER_LDAP_BIND_PASSWORD}" \
+     -e AUTH_LDAP_SEARCH_OU="${FREEIPA_USER_DN}" \
+     -e AUTH_LDAP_SEARCH_FILTER="(uid=%(user)s)" \
+     -e AUTH_LDAP_USER_ATTR_MAP='{"username": "uid", "name": "cn", "email": "mail"}' \
      -v jsdata:/opt/data \
      -v pgdata:/var/lib/postgresql \
      --tmpfs /opt/download:rw \
