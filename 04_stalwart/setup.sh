8812ca90892e6d8b0d6a143814fd85b074c39b83
Sérgio Serra <sergioserra.2003@gmail.com>
2026-03-14 16:56:47 +0000

    chore: remove alpine suffix from Stalwart mailserver Docker image tag.

--- 04_stalwart/setup.sh
+++ 04_stalwart/setup.sh
@@ -30,7 +30,7 @@ else
         -v stalwart:/opt/stalwart \
         --add-host "${KEYCLOAK_HOSTNAME}:host-gateway" \
         --add-host "${FREEIPA_HOSTNAME}:host-gateway" \
-        --name mailserver docker.io/stalwartlabs/stalwart:v0.15.5-alpine
+        --name mailserver docker.io/stalwartlabs/stalwart:v0.15.5
         
     podman network connect waf_default mailserver 2>/dev/null || true
 
